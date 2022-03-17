bf56489e791b20f79ce801a1c43bb5240a591232
CunYu <cunyu@liuli.lol>
2022-03-18 04:50:39 +0800

    feat: Config选项

--- internal/config/config.go
+++ internal/config/config.go
@@ -1,6 +1,8 @@
 package config
 
-import "github.com/BurntSushi/toml"
+import (
+	"github.com/BurntSushi/toml"
+)
 
 type (
 	Config struct {
@@ -10,16 +12,36 @@ type (
 		}
 		Plugins map[string]Plugin `toml:"plugins"`
 	}
-	Plugin struct {
-		Enabled bool `toml:"enabled"`
-	}
+	Plugin map[string]interface{}
 )
 
 var C Config
 
-func LoadConfig(path string) {
+func LoadConfig(path string) (*Config, error) {
 	_, err := toml.DecodeFile(path, &C)
 	if err != nil {
-		panic(err)
+		return nil, err
 	}
+	return &C, nil
+}
+
+func (p Plugin) Enabled() bool {
+	e, ok := p["enabled"]
+	if ok {
+		if e, ok := e.(bool); ok {
+			return e
+		}
+	}
+	return false
+}
+
+func (p Plugin) Options() map[string]interface{} {
+	m := make(map[string]interface{})
+	for k, v := range p {
+		if k == "enabled" {
+			continue
+		}
+		m[k] = v
+	}
+	return m
 }
---
+++ internal/config/config_test.go
@@ -0,0 +1,21 @@
+package config
+
+import (
+	"github.com/stretchr/testify/assert"
+	"testing"
+)
+
+func TestConfig(t *testing.T) {
+	c, err := LoadConfig("../../config.toml")
+	assert.Nil(t, err)
+
+	plugin, ok := c.Plugins["pulsoid-plugin"]
+	assert.Equalf(t, ok, true, "Plugin pulsoid-plugin not found")
+
+	enabled := plugin.Enabled()
+	assert.Equalf(t, enabled, false, "Plugin pulsoid-plugin should be disabled")
+
+	options := plugin.Options()
+	assert.NotNil(t, options)
+	assert.Equal(t, options["id"], "635e7cd8-0def-4d91-a108-c198f122f663")
+}
