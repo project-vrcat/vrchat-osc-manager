0bc6fc7a9bc21c8ebb02d4da43b43b9a5be033c4
Gizmo <cunyu@liuli.lol>
2022-06-25 17:40:46 +0800

    feat: `logger`添加`Fatal`方法

--- internal/logger/logger.go
+++ internal/logger/logger.go
@@ -2,6 +2,7 @@ package logger
 
 import (
 	"fmt"
+	"os"
 
 	"github.com/gookit/color"
 )
@@ -24,20 +25,28 @@ func Plugin(name string) *logger {
 	return _new(name, color.FgCyan)
 }
 
-func (l *logger) Error(err any) {
+func (l *logger) Error(err any) *logger {
 	_err := color.BgRed.Render(color.FgWhite.Render(" ERROR "))
-	fmt.Println(_err+l.name, err)
+	fmt.Println(l.name+_err, err)
+	return l
 }
 
-func (l *logger) Print(a ...any) {
+func (l *logger) Print(a ...any) *logger {
 	v := fmt.Sprintln(append([]any{l.name}, a...)...)
 	fmt.Print(v[:len(v)-1])
+	return l
 }
 
-func (l *logger) Println(a ...any) {
+func (l *logger) Println(a ...any) *logger {
 	fmt.Println(append([]any{l.name}, a...)...)
+	return l
 }
 
-func (l *logger) Printf(format string, a ...any) {
+func (l *logger) Printf(format string, a ...any) *logger {
 	fmt.Print(l.name, " ", fmt.Sprintf(format, a...))
+	return l
+}
+
+func (l *logger) Fatal() {
+	os.Exit(1)
 }
